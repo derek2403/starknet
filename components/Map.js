33e13d72d796476c070ca0b9028ff60fba9dd175
Cedric <Cedricctf11a@gmail.com>
2025-01-17 19:43:17 +0800

    map

---
+++ components/Map.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const Map = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  const handleWheel = (e) => {
+    const map = e.currentTarget;
+    if (e.deltaY > 0) { // Scrolling down
+      map.classList.add('zoomed-out');
+    } else { // Scrolling up
+      map.classList.remove('zoomed-out');
+    }
+  };
+
+  return (
+    <div className="fixed inset-0 w-screen h-screen overflow-hidden">
+      <div 
+        className={`
+          w-full h-full 
+          bg-[url('/background/map.png')] 
+          bg-center bg-no-repeat bg-contain
+          map-zoom-animation
+          ${isLoaded ? '' : 'scale-[2]'}
+        `}
+        style={{
+          transformOrigin: 'center center',
+        }}
+        // onWheel={handleWheel}
+      >
+        {/* Map content can be added here */}
+      </div>
+    </div>
+  );
+};
+
+export default Map;
