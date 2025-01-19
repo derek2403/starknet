6a08fddd78bb9489651fc1fc62e29388e5e6d7dc
derek2403 <derekliew0@gmail.com>
2025-01-20 02:12:54 +0800

    done cartilage and slot

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  }
+};
+
+module.exports = nextConfig; 
